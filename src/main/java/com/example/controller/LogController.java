1d458618e66b4cb388fed12c2833c9ce7a23a192
yushipeng <ysp@wozaijia.com>
2018-09-11 12:08:29 +0800

    fix

--- src/main/java/com/example/controller/LogController.java
+++ src/main/java/com/example/controller/LogController.java
@@ -1,5 +1,6 @@
-package com.example.demo;
+package com.example.controller;
 
+import com.example.demo.NeedLog;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
